442bb39835eca3e1bc6a2a850d4c5f33b4f3c349
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2023-10-02 15:09:51 -0300

    fix: ajuste nas variaveis de ambiente

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+
+const env = loadEnv(
+  'mock', 
+  process.cwd(),
+  '' 
+)
+
+const processEnvValues = {
+  'process.env': Object.entries(env).reduce(
+    (prev, [key, val]) => {
+      return {
+        ...prev,
+        [key]: val,
+      }
+    },
+    {},
+  )
+}
+
 export default defineConfig({
   plugins: [react()],
+  define: processEnvValues,
   server: {
     host: true,
     port: 5173,
