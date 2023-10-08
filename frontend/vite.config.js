ef0c45e7a291b6ec9db82b14b183d7950516870b
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2023-10-08 14:37:52 -0300

    feat: adição de google analytics

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 
@@ -21,8 +22,26 @@ const processEnvValues = {
   )
 }
 
+const env_mode = process.env.env_mode ? process.env.env_mode : 'dev'
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+  VitePluginRadar({
+    enableDev: env_mode == 'dev',
+    analytics: {
+      id: 'G-V8GQHHTLPC',
+      disabled: false,
+      send_page_view: true,
+      allow_google_signals: true,
+      allow_ad_personalization_signals: true,
+    },
+    consentDefaults: {
+      analytics_storage: 'granted',
+      ad_storage: 'denied',
+      wait_for_update: 500
+    },
+  })
+  ],
   define: processEnvValues,
   server: {
     host: true,
